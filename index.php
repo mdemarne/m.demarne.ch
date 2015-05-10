e58478d0aed88a0899f0e3e9df239006bad19d46
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-05-10 09:05:23 +0200

    Adding initial state

---
+++ index.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en">
+
+  <head>
+    <title>Mathieu Demarne</title>
+    <meta name="author" content="Mathieu Demarne" />
+  </head>
+
+  <body class="blue-grey darken-3">  
+  <nav>
+      <div class="nav-wrapper blue-grey darken-2">
+        <a href="#" class="brand-logo center">
+          Mathieu Demarne
+        </a>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-content white-text">
+              <span class="card-title small">
+                <i class="mdi-social-person text-white small"></i>
+                Enthusiast Sailor, Computer Aficionado, Traveller and Writer.
+              </span>
+            </div>
+            <div class="card-action">
+              <p><a target="blank" href="mailto:mathieu.demarne@gmail.com">mathieu.demarne@gmail.com</a></p>
+              <p><a target="blank" href="resume.pdf">Curriculum Vitae</a></p>
+              <p><a target="blank" href="http://ch.linkedin.com/pub/mathieu-demarne/83/69/b86/">LinkedIn</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-conter waves-effect waves-block waves-light" style="height:100%; background-image: url('profile01.jpg');background-size:cover;">
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-content white-text">
+              <span class="card-title">
+                <i class="mdi-hardware-mouse text-white small"></i>
+                Computer Science
+              </span>
+              <p><i>"Never trust a computer you can't throw out a window." - Steve Wozniak</i></p>
+            </div>
+            <div class="card-action">
+              <p><a target="blank" href="http://people.epfl.ch/mathieu.demarne">EPFL</a></p>      
+              <p><a target="blank" href="https://github.com/mdemarne">Github</a></p>
+              <p><a target="blank" href="http://crossstream.ch">CrossStream: an audit tool for the social web</a></p>
+              <p><a target="blank" href="http://tasti.ch">Tasti - for food lovers</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-content white-text">
+              <span class="card-title">
+                <i class="mdi-av-my-library-books text-white small"></i>
+                Writing
+              </span>
+              <p><i>"Writing, to me, is simply thinking through my fingers." -Isaac Asimov</i></p>
+            </div>
+            <div class="card-action">
+              <p><a target="blank" href="http://le-cretin-transnational.ch">Le Cr&eacute;tin Transnational</a></p>
+              <p><a target="blank" href="http://le-cretin-transnational.ch/project/et-lhomme-crea-un-homme/">Philosophical short story: <i>Et l'homme cr&eacute;a un homme</i></a></p>
+              <p><a target="blank" href="http://le-cretin-transnational.ch/project/le-songe/">Historical thriller, 1776, Britanny: <i>Le Songe</i></a></p>
+              <p><a target="blank" href="http://le-cretin-transnational.ch/project/goree/">Historical novel about loving the sea, 1772: <i>Gor&eacute;e</i></a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-conter waves-effect waves-block waves-light" style="height:100%; background-image: url('profile02.jpg');background-size:cover;">
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+         <div class="card medium">
+               <a class="twitter-timeline" href="https://twitter.com/mdemarne" data-widget-id="592618812584919041">Tweets by @mdemarne</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> 
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+         <div class="card blue-grey darken-1 medium">
+            <div class="card-content white-text">
+              <span class="card-title">
+                <i class="mdi-social-mood text-white small"></i>
+                Simply me
+              </span>
+            </div>
+            <div class="card-action">
+              <p><a target="blank" href="http://le-cretin-transnational.ch/building-cathedrals/">Why I like programming</a></p>      
+              <p><a target="blank" href="https://www.facebook.com/mdemarne">Facebook</a></p>
+              <p><a target="blank" href="https://twitter.com/mdemarne">Twitter</a></p>
+              <p><a target="blank" href="https://plus.google.com/u/0/+MathieuDemarne/">Google +</a></p>      
+              <p><a target="blank" href="https://www.quora.com/Mathieu-Demarne">Quora</a></p>
+              <p><a target="blank" href="https://www.coursera.org/user/i/c9ea6f4f7184a542cd989592a533a1da">Coursera</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+         <div class="card medium">
+          <iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Swiss%20Federal%20Institute%20of%20Technology%20Lausanne%2C%20Route%20Cantonale%2C%20Lausanne%2C%20Switzerland&key=AIzaSyCD5ntX5TGiSirjj2Bt5FnGaGFBf7-VnQs"></iframe>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-conter waves-effect waves-block waves-light" style="height:100%; background-image: url('crossstream.jpg');background-size:cover" onclick="location.href='http://www.crossstream.ch';">
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-conter waves-effect waves-block waves-light" style="height:100%; background-image: url('tasti.jpg');background-size:cover;background-position: center;" onclick="location.href='http://www.tasti.ch';">
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m6 l6">
+          <div class="card blue-grey darken-1 medium">
+            <div class="card-conter waves-effect waves-block waves-light" style="height:100%; background-image: url('lecretin.jpg');background-size:cover" onclick="location.href='http://www.le-cretin-transnational.ch';">
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <footer class="page-footer blue-grey darken-1">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">Mathieu Demarne</h5>
+            <p class="grey-text text-lighten-4">Enthusiast Sailor, Computer Aficionado, Traveller and Writer. </p>
+          </div>
+          <div class="col l4 offset-l2 s12">
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+        &copy; 2015 Mathieu Demarne
+        </div>
+      </div>
+    </footer>
+
+    <style type="text/css"> iframe[id^='twitter-widget-']{ width:100% !important; height:100% !important; }</style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-62319452-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+  </body>
+
+</html>
